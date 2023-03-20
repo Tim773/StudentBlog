2f95e835bcba5a140bf0e78c4c6d68f4e0896401
hatinurami <hati-nurami@yandex.ru>
2023-03-20 17:30:12 +0300

    --pull userdata

--- WpfApplication1/Model.Designer.cs
+++ WpfApplication1/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Создание кода T4 для модели "C:\Users\btimo\source\repos\WpfApplication1\WpfApplication1\Model.edmx" включено. 
+﻿// Создание кода T4 для модели "C:\Projects\StudentBlog\WpfApplication1\Model.edmx" включено. 
 // Чтобы включить формирование кода прежних версий, измените значение свойства "Стратегия создания кода" конструктора
 // на "Legacy ObjectContext". Это свойство доступно в окне "Свойства", если модель
 // открыта в конструкторе.
--- WpfApplication1/MyProfile.xaml.cs
+++ WpfApplication1/MyProfile.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+
 namespace WpfApplication1
 {
     /// <summary>
@@ -19,17 +20,26 @@ namespace WpfApplication1
     /// </summary>
     public partial class MyProfile : Window
     {
+       private Person user;
         public MyProfile(Person person)
         {
             InitializeComponent();
+            user = person;
 
         }
-        //Хз, что можно еще сделать в этом окне, пока не рассказано про бд. Тут и вывод можно сделать по входящему юзеру, 
-        //и вывод текстового файла в фрейм, но с фреймами надо поработать сразу со студентами.
+        
         private void changeInfo_Click(object sender, RoutedEventArgs e)
         {
             EditProfile editProfile = new EditProfile();
             editProfile.ShowDialog();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Person activeUser = AppClass.entities.Person.Where(i => i.email == user.email).FirstOrDefault();
+            userName.Text = activeUser.namePerson.ToString();
+            userLastName.Text = activeUser.fNamePerson.ToString();
+
         }
     }
 }
